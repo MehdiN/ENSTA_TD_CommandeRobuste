85fdb8f2802d8ac0b2b0e3baa4d7e7d3ae005b24
MehdiN <nima.mehdi@ensta-bretagne.org>
2017-02-01 14:06:38 +0100

    Adding matrix A

--- statesystem.m
+++ statesystem.m
@@ -1,4 +1,4 @@
-function dx = statesystem(x)
+function dx = statesystem(x,fs,r)
 
 mb=300;
 mw=60;
@@ -11,5 +11,6 @@ dx(2) = -1/mb*(ks*(x(1)-x(3))+bs*(x2-x4)-fs);
 dx(3) = x(4);
 dx(4) = 1/mw *(ks*(x(1)-x(3))+bs*(x2-x4)-kt*(x(3)-r)-fs);
 
-
 end
+
+A = [0 1 0 0;-k/mb -bs/mb ks/mb bs/mb;0 0 0 1;ks/mw ks/mw -bs/mw-kt -bs/mw];

a93b3efa86d175b44f0687975c890d4fbe8c166a
MehdiN <nima.mehdi@ensta-bretagne.org>
2017-02-01 13:52:42 +0100

    hello

--- statesystem.m
+++ statesystem.m
@@ -1,9 +1,11 @@
 function dx = statesystem(x)
 
-dx(1) = x(2)
-dx(2) = 
-dx(3) =
-dx(4) =
+mb
+
+dx(1) = x(2);
+dx(2) = -1/mb*(ks*(x(1)-x(3))+bs*(x2-x4)-fs);
+dx(3) = x(4);
+dx(4) = 1/mw *(ks*(x(1)-x(3))+bs*(x2-x4)-kt*(x(3)-r)-fs);
 
 
 end

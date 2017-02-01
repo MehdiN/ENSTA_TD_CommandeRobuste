0ae7c1f4ae4c425c899d437d6aa416bd30638717
simchanu29 <simon.chanu@ensta-bretagne.org>
2017-02-01 15:26:32 +0100

    before merge

--- statesystem.m
+++ statesystem.m
@@ -2,38 +2,35 @@
 
 mb=300; %kg
 mw=60; %kg
-bs=1000;
-ks=16000;
-kt=19000;
+bs=1000; %N/m/s
+ks=16000; %N/m
+kt=190000; %N/m
 
-% <<<<<<< Updated upstream
-% dx(1) = x(2);
-% dx(2) = -1/mb*(ks*(x(1)-x(3))+bs*(x2-x4)-fs);
-% dx(3) = x(4);
-% dx(4) = 1/mw *(ks*(x(1)-x(3))+bs*(x2-x4)-kt*(x(3)-r)-fs);
-% =======
-% 
-% >>>>>>> Stashed changes
+A = [0      1      0         0;
+    -ks/mb -bs/mb  ks/mb     bs/mb;
+     0      0      0         1;
+     ks/mw  bs/mw -(ks+kt)/mw -bs/mw];
 
+B = [ 0      0;
+      0      1/mb;
+      0      0;
+      kt/mw -1/mw];
 
-A = [0 1 0 0;
-    -ks/mb -bs/mb ks/mb bs/mb;
-    0 0 0 1;
-    ks/mw ks/mw -bs/mw-kt -bs/mw];
-B = [ 0 0;
-      0 1/mb;
-      0 0;
-       kt/mw -1/mw];
-
-C = [1 0 0 0;
-    1 0 -1 0;
+C = [1      0     0     0;
+     1      0    -1     0;
     -ks/mb -bs/mb ks/mb bs/mb];
 
-D = [0 0;0 0;0 1/mb];
+D = [0 0    ;
+     0 0    ;
+     0 1/mb];
 
 sys = ss(A,B,C,D);
 sys.InputName = {'r','fs'};
 sys.OutputName = {'xb','sd','ab'};
-z= tzero(sys);
+
+z_sys1 = tzero( sys({'xb','ab'},'fs') );
+z_sys2 = tzero( sys('sd','fs') );
+
 tfsys = tf(sys);
-bodeplot(sys)
+bodeplot(sys({'ab','sd'},{'fs','r'}))
+

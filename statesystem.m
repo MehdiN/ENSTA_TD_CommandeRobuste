618679a8667ad1c8c839b4f50ad30e7e12978577
simchanu29 <simon.chanu@ensta-bretagne.org>
2017-02-08 15:19:55 +0100

    Question 8

--- statesystem.m
+++ statesystem.m
@@ -20,6 +20,7 @@ B = [ 0      0;
       kt/mw -1/mw];
 
 C = [1      0     0     0;
+C = [1      0     0     0;
      1      0    -1     0;
     -ks/mb -bs/mb ks/mb bs/mb];
 
@@ -40,26 +41,34 @@ figure;
 bodeplot(sys({'ab','sd'},{'fs','r'}))
 
 %% Hydrolique
+% On a sys c'est la grosse boiboite
+% act c'est le filtre de l'actionneur
 
-act = tf(1,[1/60 1])
+actNom = tf(1,[1/60 1])
 % actSys = tf2ss(act)
 
 Wunc = makeweight(0.4,15,20)
+figure()
+bodeplot(Wunc,'r')
+title('Wunc')
+
 unc = ultidyn('unc',[1 1])
-actSys = act*(1+Wunc*unc)
+Act = actNom*(1+Wunc*unc)
 
-actSys.InputName = {'u'}
-actSys.OutputName = {'fs'}
-
-figure;
-bodeplot(actSys,act,'r-+')
-
-sysTot = connect(sys,actSys,{'r','u'},{'xb','sd','ab'})
+Act.InputName = {'u'}
+Act.OutputName = {'fs'}
 
 figure;
-bodeplot(sysTot)
+bodeplot(Act,actNom,'r-+')
+title('Famille de Act')
 
+Sys_sys_act = connect(sys,actSys,{'r','u'},{'xb','sd','ab'})
+
+figure;
+bodeplot(Sys_sys_act)
+title('La réponse du système et son actionneur')
 %% H infini 
+% On modélise les W les poids a quache entrée ou sortie.
 Wroad = ss(0.07)
 Wroad.u = 'd1'
 Wroad.y = 'r'
@@ -77,16 +86,22 @@ Wd3.u = 'd3'
 Wd3.y = 'Wd3'
 
 %% Question 8
+% Normalement on prend une boucle fermée
 
-HandlingTarget = 0.04 * tf([1/8 1],[1/80 1]);
-ComfortTarget = 0.4 * tf([1/0.45 1],[1/150 1]);
-Targets = [HandlingTarget; ComfortTarget];
+bode(Sys_sys_act({'ab','sd'},'r'))
+title('Système en boucle ouverte')
+
+Wsd = makeweight(0.5,60,30)
+Wab = makeweight(0.5,60,30)
+% HandlingTarget = 0.04 * tf([1/8 1],[1/80 1]);
+% ComfortTarget = 0.4 * tf([1/0.45 1],[1/150 1]);
+% Targets = [HandlingTarget; ComfortTarget];
 
 %% Question 9
 
-bodemag(sysTot({'sd','ab'},'r')*Wroad,'b',Targets,'r--',{1,1000})
-grid, title('Response to road disturbance')
-legend('Open-loop','Closed-loop target')
+% bodemag(sysTot({'sd','ab'},'r')*Wroad,'b',Targets,'r--',{1,1000})
+% grid, title('Response to road disturbance')
+% legend('Open-loop','Closed-loop target')
 %
 
 %% Question 10
